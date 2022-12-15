3a5009e65ed50d715cb3c11b0d3a12b8ddcb22cb
Nafis.3000 <nafis.3000@gmail.com>
2022-12-14 19:43:22 -0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Script from 'next/script'
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Component {...pageProps} />
+  <Script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js" />
+  </>
+  )
 }
 
 export default MyApp
+
